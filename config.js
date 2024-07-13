ce6493bde00968ef97280cfe94f4afca3d1df517
Rishi1Heart1Maker <168374527+Rishi1Heart1Maker@users.noreply.github.com>
2024-07-13 21:53:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaijE4GIyPtVx0sf083E'
+global.linkGroup = 'https://chat.whatsapp.com/LNLZ7AT1CQpHDOWU259LYK'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "Rishi Heart Maker";
+global.owner = ['923252132556'];
+global.premium = ['923252132556'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "Rishi Heart Maker";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
+global.available = true 
+global.autoTyping = true 
+global.autoRecord = true 
+global.autoreact = true 
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false 
  
 /*REPLY MESSAGE*/
 global.mess = {
